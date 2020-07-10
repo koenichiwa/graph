9722379ed58b718f98f7a4cfe2eb3dc469b4d034
Koen <koenvwel@gmail.com>
2020-07-10 11:29:26 +0200

    Start on documentation

--- src/commonMain/kotlin/graphs/baseinterfaces/GraphBase.kt
+++ src/commonMain/kotlin/graphs/baseinterfaces/GraphBase.kt
@@ -1,6 +1,14 @@
 package graphs.baseinterfaces
 
+/**
+ * Base interface where each graph is derived from
+ */
 interface GraphBase<Vertex> {
+    /**
+     * If this graph is directed this will be true,
+     * e.g. when there is a path from A to B then
+     * that doesn't mean necessarily that there is a path from B to A
+     */
     val isDirected: Boolean
 
     fun containsVertex(vertex: Vertex): Boolean
