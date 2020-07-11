f039f27344ab5925d194093d4d70f6682c3e35b8
Koen <koenvwel@gmail.com>
2020-07-11 18:32:44 +0200

    Testing github actions change

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,6 @@ repositories {
 }
 
 kotlin {
-    /* Targets configuration omitted. 
-    *  To find out how to configure the targets, please follow the link:
-    *  https://kotlinlang.org/docs/reference/building-mpp-with-gradle.html#setting-up-targets */
     jvm()
 
     sourceSets {
