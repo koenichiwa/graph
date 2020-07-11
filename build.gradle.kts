481b4b2e3218b70c9046e1f62f609a4140f4d727
Koen <koenvwel@gmail.com>
2020-07-11 19:28:04 +0200

    Fuck around with js and linux builds

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("multiplatform") version "1.3.72"
+    id("com.github.roroche.plantuml") version "1.0.2"
 }
 
 group = "com.koenichiwa"
@@ -11,8 +12,13 @@ repositories {
 
 kotlin {
     jvm()
+    js()
+//    macosX64()
+    linuxArm64()
+    linuxX64()
 
     sourceSets {
+        // Common
         val commonMain by getting {
             dependencies {
                 implementation(kotlin("stdlib-common"))
@@ -24,6 +30,9 @@ kotlin {
                 implementation(kotlin("test-annotations-common"))
             }
         }
+
+        // Build Types
+        // JVM
         jvm().compilations["main"].defaultSourceSet {
             dependsOn(commonMain)
             dependencies {
@@ -36,5 +45,34 @@ kotlin {
                 implementation(kotlin("test-junit"))
             }
         }
+
+        // Javascript
+        js().compilations["main"].defaultSourceSet {
+            dependsOn(commonMain)
+            dependencies {
+                implementation(kotlin("stdlib-js"))
+            }
+        }
+
+        js().compilations["test"].defaultSourceSet {
+            dependsOn(commonTest)
+            dependencies {
+                implementation(kotlin("kotlin-test-js"))
+            }
+        }
+
+        // Mac
+//        macosX64().compilations["main"].defaultSourceSet {
+//            dependsOn(commonMain)
+//        }
+
+        linuxX64().compilations["main"].defaultSourceSet {
+            dependsOn(commonMain)
+        }
     }
+}
+
+classDiagram {
+    packageName = "com.koenichiwa.graphs"
+    outputFile = project.file("/diagrams/class_diagram.plantuml")
 }
